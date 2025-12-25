206bd7bb59c4eacd5efc07f6282aeef3c77ef4cb
Aditya Shah <aditya03714@gmail.com>
2025-12-25 21:33:44 +0530

    feat: implement submission repository

---
+++ src/repositories/submission.repository.ts
@@ -0,0 +1,26 @@
+import { Submission } from '../models/submission.model';
+import { ISubmission } from '../types/challenge.types';
+
+export class SubmissionRepository {
+  async create(data: Partial<ISubmission>): Promise<ISubmission> {
+    return Submission.create(data);
+  }
+
+  async findByChallenge(challengeId: string): Promise<ISubmission[]> {
+    return Submission.find({ challengeId }).populate('userId', 'name email');
+  }
+
+  async findByUser(userId: string): Promise<ISubmission[]> {
+    return Submission.find({ userId }).populate('challengeId', 'title ecoPoints');
+  }
+
+  async findById(id: string): Promise<ISubmission | null> {
+    return Submission.findById(id).populate('challengeId userId');
+  }
+
+  async update(id: string, data: Partial<ISubmission>): Promise<ISubmission | null> {
+    return Submission.findByIdAndUpdate(id, data, { new: true, runValidators: true });
+  }
+}
+
+export const submissionRepository = new SubmissionRepository();
